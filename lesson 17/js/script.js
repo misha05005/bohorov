fe02c643a3080498a65d206d21e47f7c998a4160
Богоровський Михаил <misha05005@gmail.com>
2020-08-31 20:27:40 +0300

    lesson17

---
+++ lesson 17/js/script.js
@@ -0,0 +1,18 @@
+/*площадь прямоугольника*/ 
+let a = "7"
+let b = "3"
+let rectangle = a * b
+console.log(rectangle);
+
+/*площадь круга*/
+
+const p = "3.14"
+let r = "3"
+let circle =  p*(r*r)
+console.log(circle);
+
+/*число в сепень*/
+let number = "10"
+let step = (number) ** (number)
+
+console.log(step);
