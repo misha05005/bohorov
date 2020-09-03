cd38923d097da4a16501515e4cf8f5c09f45180f
Богоровський Михаил <misha05005@gmail.com>
2020-09-03 18:50:05 +0300

    lesson18

---
+++ lesson 18/assets/scripts/scrips.js
@@ -0,0 +1,16 @@
+/*Ask*/
+    let surname = prompt("Ваша Фамилия");
+    let name = prompt("Ваше имя");
+    let age = prompt("Ваш возраст");
+/*Ask*/
+
+/*Age*/
+    const year =2020;
+    let userage = year - age ;
+/*Age*/ 
+/*record*/
+let doc = document;
+doc.getElementById('surname').innerText = surname;
+doc.getElementById('name').innerText = name;
+doc.getElementById('age').innerText = age;
+/*record*/
