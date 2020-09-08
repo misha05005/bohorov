693ca453a4056ce83e3fba8c4cb1ea446c5ba494
Богоровський Михаил <misha05005@gmail.com>
2020-09-08 18:46:49 +0300

    lesson19

---
+++ lesson19/script/script.js
@@ -0,0 +1,38 @@
+/*10 имён*/ 
+let names = [
+    'Юля',
+    'Надя',
+    'Ира',
+    'Лиза',
+    'Женя', 
+    'Миша', 
+    'Игорь', 
+    'Максим', 
+    'Саша', 
+    'Виталик'
+];
+/*вывод на экран*/
+    console.log(names);
+    for (let i = 0; i <names.length; i++ ) {
+        console.log('ЕЛЕМЕНТ [' + i + ']: ' + names[i] ) ;
+    }
+
+    /*while*/ 
+    while ( k < names.length) {
+        console.log ('елемент с индексом [' + k + ']: ' + names [k]);
+        k++;
+    }
+/*output element*/
+console.log("елемент  с индексом");
+console.log( 'елемент с индексом [0]: ' + names[0]);
+console.log( 'елемент с индексом [0]: ' + names[1]);
+console.log( 'елемент с индексом [0]: ' + names[2]);
+console.log( 'елемент с индексом [0]: ' + names[3]);
+console.log( 'елемент с индексом [0]: ' + names[4]);
+console.log( 'елемент с индексом [0]: ' + names[5]);
+console.log( 'елемент с индексом [0]: ' + names[6]);
+console.log( 'елемент с индексом [0]: ' + names[7]);
+console.log( 'елемент с индексом [0]: ' + names[8]);
+console.log( 'елемент с индексом [0]: ' + names[9]);
+
+
