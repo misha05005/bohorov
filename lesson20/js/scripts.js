192d21b5d3f4aa15963b15514cc1c2baf2ab29d0
Богоровський Михаил <misha05005@gmail.com>
2020-09-15 19:27:23 +0300

    lesson20_1

--- lesson20/js/scripts.js
+++ lesson20/js/scripts.js
@@ -55,36 +55,35 @@ let cars = {
         age :'2009' , 
         price : '7490' },
     car_9: {
-        img: 'img/img-10.webp' 
-        ,car:'BMW',
+        img: 'img/img-10.webp',
+        car:'BMW',
         model:'X5 M', 
         age :'2019' , 
-        price : '92000' },
+        price : '92000' }
 };
 
 console.log(cars);
 
-let div = document.createElement(tagName:'div');
+let div = document.createElement('div');
 div.className = 'cars-list';
+document.body.append(div);
 
 for (let key in cars) {
-    let div.child = document.createElement(tagName:'div');
-    div.child.className = 'cars-list_item'; 
-    div.child.innerHTML = '' + 
-        <div class = 'cars-list_item-area'> +
-            <div class = 'cars-list_item-img'>+
-                <img src="' + cars[key].img + cars[key].car + " >
-            </div> +
-            <div class="cars-list_item-content"> +
-                '<h2>' + cars[key].car + '' + cars[key].madel +'</h2>'+
-                '<div class='cars-list_item-price'>' +
-                    price_symbol + '' + cars[key].price +
-                '</div>'
-                '<div class='cars-list_item-age'>' +
-                  'год' + cars[key].age +
-                '</div>'
+    let child = document.createElement('div');
+    child.className = 'cars-list_item';
+    child.innerHTML = `<div class="cars-list_item-area">
+            <div class="cars-list_item-img">
+                <img src="${cars[key].img}" title="${cars[key].car}">
             </div>
-        </div>' +  
-    '';
-    div.append(div.child)
+            <div class="cars-list_item-content">
+                <h2>${cars[key].car} ${cars[key].model}</h2>
+                <div class="cars-list_item-price">
+                    ${price_symbol} ${cars[key].price}
+                </div>
+                <div class="cars-list_item-age">
+                  год ${cars[key].age}
+                </div>
+            </div>
+        </div>`;
+    div.append(child);
 }
