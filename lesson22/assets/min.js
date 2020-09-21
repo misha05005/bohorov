11cdf479042a580b165eb4756bb62069929b0db8
Богоровський Михаил <misha05005@gmail.com>
2020-09-21 19:24:04 +0300

    lesson22

---
+++ lesson22/assets/min.js
@@ -0,0 +1,15 @@
+let moeny_day = (day) => {
+    return +prompt(day);
+}
+console.log(moeny_day(`Ваш дневной зароботок`));
+
+let calculation = (one_day , days) => {
+    return  =  days *  one_day ;
+}
+const all_days = 25 ;
+
+let all_calc = calculation(all_days,()=>{
+    moeny_day(`Ваш дневной зароботок`);
+});
+
+alert(`В конце месяца вы получите :${all_cal})
